0710ac3a940307f3a53932fdf64ed4d1c98447f9
SelenaMiao <ym623@cornell.edu>
2025-10-19 02:18:09 +0000

    my first commit

---
+++ commit.R
@@ -0,0 +1,9 @@
+install.packages("gert")
+install.packages("credentials")
+
+library(gert)
+library(credentials)
+credentials::set_github_pat()
+
+gert::git_add("cimmit.R")
+gert::git_commit_all(".")
