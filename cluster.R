7196bc89d7b369ec036624bc3e8857305e29562e
jl4584 <jl4584@cornell.edu>
2025-10-20 00:47:10 +0000

    my first commit

---
+++ cluster.R
@@ -0,0 +1,16 @@
+install.packages(c("gert", "credentials"))
+library(gert)
+library(credentials)
+
+credentials::set_github_pat()
+
+# this will prompt a popup that asks you to enter your GitHub Personal Access Token.
+
+gert::git_pull() # pull most recent changes from GitHub
+gert::git_add(dir(all.files = TRUE)) # select any and all new files created or edited to be 'staged'
+gert::git_add("C:/Users/liuji/OneDrive/Documents/CEE 5930/cluster_analysis.R")
+# 'staged' files are to be saved anew on GitHub 
+file.exists("C:/Users/liuji/OneDrive/Documents/CEE 5930/cluster_analysis.R") 
+gert::git_commit_all("my first commit") # save your record of file edits - called a commit
+
+gert::git_push() # push your commit to GitHub
